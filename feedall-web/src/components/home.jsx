a7dfa35aecb633f6017a111b5da40b41b40600cb
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2024-03-03 22:06:09 +0100

    fix arrows

--- feedall-web/src/components/home.jsx
+++ feedall-web/src/components/home.jsx
@@ -3,6 +3,8 @@ import asideImage from "/src/feedallImg.jpg";
 import "./homePage.scss";
 import { Link } from "react-router-dom";
 import NavBar from "./nav";
+import {motion} from 'framer-motion'
+import { FaLongArrowAltRight } from "react-icons/fa";
 function HomePage() {
   return (
     <>
@@ -11,27 +13,106 @@ function HomePage() {
       <div className="wrapper">
         <div className="home-container">
           <div className="hero-description">
-            <h1>
+          <motion.h1
+            initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delayChildren: 1,
+                  delay: 1,
+                  duration: 1.5,
+                  type: "tween",
+                },
+              }}
+          >
               A truly circular
               <div>food</div>
               production platform
-            </h1>
+            </motion.h1>
             <div className="image-background-mobile">
               <img  className='image-background-mobile' src={asideImage} width={340} alt="feedall" />
             </div>
           </div>
           <div className="links">
             <Link to="/join">
-              <h1>Join Our Team</h1>
+               <motion.h1
+            initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delayChildren: 1,
+                  delay: 0.7,
+                  duration: .5,
+                  type: "tween",
+                },
+              }}
+          >Join Our Team </motion.h1>
+          <FaLongArrowAltRight width={100} />
             </Link>
             <Link to="/invest">
-              <h1>Become an Investor</h1>
+              <motion.h1
+            initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delayChildren: 1,
+                  delay: 0.8,
+                  duration: .5,
+                  type: "tween",
+                },
+              }}>Become an Investor</motion.h1>
+                        <FaLongArrowAltRight width={100} />
+
             </Link>
             <Link to="/join-cohort">
-              <h1>Join The Next Cohort</h1>
+              <motion.h1
+            initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delayChildren: 1,
+                  delay: 0.9,
+                  duration: .5,
+                  type: "tween",
+                },
+              }}>Join The Next Cohort </motion.h1>
+                        <FaLongArrowAltRight width={100} />
+
             </Link>
             <Link to="/faq">
-              <h1>FAQ</h1>
+              <motion.h1
+            initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delayChildren: 1,
+                  delay: 1,
+                  duration: .5,
+                  type: "tween",
+                },
+              }}>FAQ</motion.h1>
+                        <FaLongArrowAltRight width={100} />
+
             </Link>
           </div>
         </div>
